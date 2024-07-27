646d774ff491f4af0de215bb5092267f3df93d36
jasonyu17 <jason.yu171@gmail.com>
2024-07-26 20:43:09 -0400

    edit to application

--- library/src/test/java/gsu/edu/library/LibraryApplicationTests.java
+++ library/src/test/java/gsu/edu/library/LibraryApplicationTests.java
@@ -3,7 +3,7 @@ package gsu.edu.library;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = LibraryApplication.class)
 class LibraryApplicationTests {
 
 	@Test
